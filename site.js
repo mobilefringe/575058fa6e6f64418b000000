18ceca89d8ea3972266a2f646b60a727da216e69
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-13 12:33:45 -0400

    Creekside Town Center 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Creekside Towne Center",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57ffb65e6e6f642821490000/image/jpeg/undefined/ctc_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "instagram" : "196558634"
+                }
