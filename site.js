769208cd033f645e1e9873d045b39490862928c3
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-12-29 12:29:52 -0500

    Creekside Town Center 1.2  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Creekside Towne Center",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/57ffb65e6e6f642821490000/image/jpeg/undefined/ctc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "//creekside.mallmaverick.com/api/v2/creekside/social.json"
+                    "social_feed" : "//creekside.mallmaverick.com/api/v2/creekside/social.json"
                 }
